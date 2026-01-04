e2167185d10b9589c9c459fc37be44878e8e0754
Janaka Chathuranga <bjchathuranga@gmail.com>
2026-01-05 02:46:05 +0800

    Update clip config

--- configs/train_config.py
+++ configs/train_config.py
@@ -52,7 +52,7 @@ def get_config(config_string):
                     N=16,
                     discount=0.99,                    
                     tanh_scale=1.0,
-                    vh_clip=50
+                    vh_clip=25
                 ),
                 dataset_kwargs=dict(
                     **base_data_config,
