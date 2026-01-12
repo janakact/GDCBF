afe65cfee7552ca614d3e9ac891c3c4e50256704
Janaka Chathuranga <bjchathuranga@gmail.com>
2026-01-12 13:55:49 +0800

    Add outlier percentage

--- jaxrl5/data/dsrl_datasets.py
+++ jaxrl5/data/dsrl_datasets.py
@@ -8,7 +8,7 @@ import h5py
 
 
 class DSRLDataset(Dataset):
-    def __init__(self, env: gym.Env, clip_to_eps: bool = True, eps: float = 1e-5, critic_type="qc", data_location=None, cost_scale=1., ratio = 1.0, noise_scale=0.0):
+    def __init__(self, env: gym.Env, clip_to_eps: bool = True, eps: float = 1e-5, critic_type="qc", data_location=None, cost_scale=1., ratio = 1.0, noise_scale=None, outliers_percent=None):
 
         if data_location is not None:
             # Point Robot
@@ -33,7 +33,7 @@ class DSRLDataset(Dataset):
             if ratio == 1.0:
                 dataset_dict = env.get_dataset()
                 dataset_dict = env.pre_process_data(dataset_dict,
-                                noise_scale=noise_scale)
+                                noise_scale=noise_scale, outliers_percent=outliers_percent)
             else:
                 _, dataset_name = os.path.split(env.dataset_url)
                 file_list = dataset_name.split('-')
--- train_offline.py
+++ train_offline.py
@@ -31,6 +31,7 @@ flags.DEFINE_integer('mode', 1, 'Mode for training')
 flags.DEFINE_integer('seed', 1, 'Seed')
 flags.DEFINE_float('eval_temperature', 0.0, 'Temperature for evaluation')
 flags.DEFINE_float('noise_scale', 0.0, 'Noise Scale')
+flags.DEFINE_float('outliers_percent', 0.0, 'Outliers Percent')
 # flags.DEFINE_integer('max_steps', 500_001, 'max steps')
 # flags.DEFINE_integer('eval', 10000, 'eval steps')
 flags.DEFINE_string('project', '081125', 'Name of the experiment')
@@ -55,6 +56,7 @@ def call_main(details, env_id):
     config_for_wandb = to_dict(details['agent_kwargs'])
     wandb.init(project=details['project'], name=details['experiment_name'], group=details['group'], config=config_for_wandb)
     noise_scale = details['agent_kwargs']['noise_scale']
+    outliers_percent = details['agent_kwargs']['outliers_percent']
 
     # wandb.init( name=details['experiment_name'], group=details['group'], config=config_for_wandb)
     if details['env_name'] == 'PointRobot':
@@ -66,13 +68,15 @@ def call_main(details, env_id):
     else:
 
         env = gym.make(details['env_name']) #,use_render=True)
+        env.set_target_cost(details['agent_kwargs']['cost_limit'])
         # ds = DSRLDataset(env, critic_type=details['agent_kwargs']['critic_type'], cost_scale=details['dataset_kwargs']['cost_scale'], ratio=details['ratio'])
-        print("Noise scale:", noise_scale)
-        ds = DSRLDataset(env, cost_scale=details['dataset_kwargs']['cost_scale'], noise_scale=noise_scale)#, ratio=details['ratio'])
+        print("Noise scale:", noise_scale, "Outliers: ", outliers_percent)
+        ds = DSRLDataset(env, cost_scale=details['dataset_kwargs']['cost_scale'], noise_scale=noise_scale, outliers_percent=outliers_percent)#, ratio=details['ratio'])
         env_max_steps = env._max_episode_steps
         env = wrap_gym(env, cost_limit=details['agent_kwargs']['cost_limit'])
         env = OfflineEnvWrapper(env)
-        env.set_noise_scale(noise_scale)
+        if noise_scale is not None:
+            env.set_noise_scale(noise_scale)
         ds.normalize_returns(env.max_episode_reward, env.min_episode_reward, env_max_steps)
     # ds.seed(details['dataset_kwargs']["seed"])
     ds.seed(details["seed"])
@@ -83,6 +87,7 @@ def call_main(details, env_id):
     model_cls = config_dict.pop("model_cls") 
     config_dict.pop("cost_scale") 
     config_dict.pop("noise_scale") 
+    config_dict.pop("outliers_percent") 
     agent = globals()[model_cls].create(
         details['seed'], env.observation_space, env.action_space, **config_dict
     )
@@ -117,7 +122,8 @@ def main(_):
     env_id = FLAGS.env_id
     parameters['agent_kwargs']['mode'] = FLAGS.mode
     parameters['agent_kwargs']['eval_temperature'] = FLAGS.eval_temperature
-    parameters['agent_kwargs']['noise_scale'] = FLAGS.noise_scale
+    parameters['agent_kwargs']['noise_scale'] = FLAGS.noise_scale if FLAGS.noise_scale > 0.0 else None
+    parameters['agent_kwargs']['outliers_percent'] = FLAGS.outliers_percent if FLAGS.outliers_percent > 0.0 else None
     parameters['seed'] = FLAGS.seed
     # mode = FLAGS.mode
     algo = 'fisor' #if mode == 1 else 'tanh'
