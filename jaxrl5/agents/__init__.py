12b008267bd2cf29aa22a570d0ce37d08aad648f
Janaka <janakactsl@gmail.com>
2026-05-16 13:56:14 +0800

    Add gp-pages index-html

--- jaxrl5/agents/__init__.py
+++
@@ -1,9 +0,0 @@
-from jaxrl5.agents.fisor.fisor import FISOR
-from jaxrl5.agents.fisor.cbf import CBF
-
-# from FISOR.jaxrl5.agents.fisor.cgpt import CBF
-
-# from jaxrl5.agents.gdcbf import CBFMLP, CBFTrainState, train_cbf, train_step,update
-# from jaxrl5.agents.gdcbf import GDCBF
-# from jaxrl5.agents.dcbf import DCBF
-
--- sweep.py
+++
@@ -1,8 +0,0 @@
-import wandb
-import yaml
-
-with open("sweep.yaml", "r") as f:
-    sweep_config = yaml.safe_load(f)
-
-sweep_id = wandb.sweep(sweep=sweep_config, project="FISOR")
-wandb.agent(sweep_id, count=5)
